87688da7561e2a091c1a043a6e5f9bb752a68eed
habib054 <habiburrahman0540@gmail.com>
2021-04-23 22:20:51 +0400

    Burger component added

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxs';
+import Burger from "../../components/Burger/Burger"
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Aux>
+                    <Burger/>
+                <div>Burger controller</div>
+            </Aux>
+        )
+    }
+}
+export default BurgerBuilder;
